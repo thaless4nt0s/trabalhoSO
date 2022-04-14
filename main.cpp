56ebb1c2ba87e538e961a5933ab37f59c64d0fae
= <=>
2022-04-14 09:59:09 -0300

    Shell simplificado

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    while(1){
+        char s[100];
+        printf("escreva_aqui: ");
+        scanf("%s",s);
+        system(s);
+    }
+
+    return 0;
+}
